3c40e546241bed2b683370c00de878b4bec35315
Army-R <jarupa310@gmail.com>
2025-11-24 16:19:14 -0600

    Create test title file

---
+++ validateTitle.py
@@ -0,0 +1,27 @@
+# Selenium Tutorial #1
+# Validate the title of the website
+
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+# Set variables
+title = "Newgrounds.com — Everything, By Everyone"
+
+# Path to the EdgDriver 
+PATH = r"C:\Program Files (x86)\edgedriver_win64\msedgedriver.exe"
+
+# Create a service object
+service = Service(executable_path=PATH)
+
+# Initialize the EdgDriver with the service
+driver = webdriver.Edge(service=service)
+
+# Open the website
+driver.get("https://newgrounds.com")
+
+# Validate the title
+if driver.title == title:
+    print("Title is correct")
+    driver.quit()
+else:
+    print("Title does not match, current title is: ", driver.title)
