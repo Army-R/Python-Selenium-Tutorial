94e8b7f4a5b611b3f834e25f991c2754c9170f0a
Army-R <80074550+Army-R@users.noreply.github.com>
2025-11-26 10:17:20 -0600

    Improve the  driver steps and quit the browser at the end

--- validateTitle.py
+++ validateTitle.py
@@ -1,20 +1,14 @@
 # Selenium Tutorial #1
 # Validate the title of the website
 
+# Import webdriver module
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
 
 # Set variables
 title = "Newgrounds.com — Everything, By Everyone"
 
-# Path to the EdgDriver 
-PATH = r"path\msedgedriver.exe"
-
-# Create a service object
-service = Service(executable_path=PATH)
-
-# Initialize the EdgDriver with the service
-driver = webdriver.Edge(service=service)
+# Initialize the EdgDriver
+driver = webdriver.Edge()
 
 # Open the website
 driver.get("https://newgrounds.com")
@@ -22,6 +16,8 @@ driver.get("https://newgrounds.com")
 # Validate the title
 if driver.title == title:
     print("Title is correct")
-    driver.quit()
 else:
     print("Title does not match, current title is: ", driver.title)
+
+# Exit the browser
+driver.quit()
