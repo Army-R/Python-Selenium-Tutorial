01c1dda864a3cdd1b7e5d3261287ba39bfb63cd8
Army-R <80074550+Army-R@users.noreply.github.com>
2025-11-28 14:08:39 -0600

    Create a class for the locators of the results web page

--- unitTestFramework/locator.py
+++ unitTestFramework/locator.py
@@ -7,3 +7,7 @@ from selenium.webdriver.common.by import By
 # Class to define all locators for the Main Page
 class MainPageLocators(object):
     GO_BUTTON = (By.ID, "submit") # Capitalized to indicate constants
+
+# Class for the results page
+class SearchResultsPageLocators(object):
+    pass
